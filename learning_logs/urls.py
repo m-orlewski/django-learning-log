b88ca01a581e5a538f62c0b2b44a083591ecb57c
m-orlewski <morlewski@protonmail.com>
2021-07-27 16:52:56 +0200

    add subpages for topics

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,5 +6,7 @@ from . import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='index'), #home page
+    path('topics/', views.topics, name='topics'), # page showing all topics
+    path('topics/<int:topic_id>/', views.topic, name='topic'), # page for a single topic
 ]
