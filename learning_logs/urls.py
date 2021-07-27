57c969becdb6bc10607e2f10793a7a91764935a6
m-orlewski <morlewski@protonmail.com>
2021-07-27 16:15:22 +0200

    add home page

---
+++ learning_logs/urls.py
@@ -0,0 +1,10 @@
+"""URL patterns for learning_logs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'learning_logs'
+urlpatterns = [
+    path('', views.index, name='index'),
+]
